2d05919b75249410dacbe6a65bdedc6c4e4a5995
kober <thorsten.kober@gmail.com>
2019-04-14 19:06:16 +0200

    GSON fix

--- video-debrief-core/src/main/java/de/kobair/videodebrief/core/event/EventPojo.java
+++ video-debrief-core/src/main/java/de/kobair/videodebrief/core/event/EventPojo.java
@@ -5,14 +5,15 @@ import java.util.List;
 import java.util.Map;
 
 import de.kobair.videodebrief.core.perspective.Perspective;
+import de.kobair.videodebrief.core.perspective.PerspectivePojo;
 
 public class EventPojo implements Event {
 	
 	private String name;
 	private String subPath;
-	private Map<String, Perspective> perspectives;
+	private Map<String, PerspectivePojo> perspectives;
 	
-	public EventPojo(String name, String subPath, Map<String, Perspective> perspectives) {
+	public EventPojo(String name, String subPath, Map<String, PerspectivePojo> perspectives) {
 		this.name = name;
 		this.subPath = subPath;
 		this.perspectives = perspectives;
@@ -36,11 +37,11 @@ public class EventPojo implements Event {
 		this.subPath = subPath;
 	}
 	
-	public Map<String, Perspective> getPerspectives() {
+	public Map<String, PerspectivePojo> getPerspectives() {
 		return perspectives;
 	}
 	
-	public void setPerspectives(Map<String, Perspective> perspectives) {
+	public void setPerspectives(Map<String, PerspectivePojo> perspectives) {
 		this.perspectives = perspectives;
 	}
 
