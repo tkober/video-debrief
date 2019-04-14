17375a7f772f653b1cb2e09b6209abf43547b5a8
tkober <thorsten.kober@gmail.com>
2019-04-14 19:18:44 +0200

    UI optimization

--- video-debrief-ui/src/main/java/de/kobair/videodebrief/ui/App.java
+++ video-debrief-ui/src/main/java/de/kobair/videodebrief/ui/App.java
@@ -39,6 +39,7 @@ public class App extends Application {
 		Stage stage = getPrimaryStage();
 		WorkspaceController controller = this.loadScene(stage, "Workspace.fxml", WorkspaceController.class);
 		controller.setWorkspace(workspace);
+//		stage.setMaximized(true);
 		stage.show();
 	}
 	
