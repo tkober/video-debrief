db1614052bd087cf82bdc91128dd17e5593f6967
kober <thorsten.kober@gmail.com>
2019-04-16 23:20:20 +0200

    Fixed Dcf directory regex

--- video-debrief-core/src/main/java/de/kobair/videodebrief/core/media/dcf/Dcf.java
+++ video-debrief-core/src/main/java/de/kobair/videodebrief/core/media/dcf/Dcf.java
@@ -3,6 +3,6 @@ package de.kobair.videodebrief.core.media.dcf;
 class Dcf {
 	
 	static final String DCIM_DIRECTORY_NAME = "DCIM";
-	static final String DCF_DIRECTORY_REGEX = "(^([0-9]{3})([0-9,a-z,A-Z]{5})$)";
+	static final String DCF_DIRECTORY_REGEX = "(^([0-9]{3})([0-9,a-z,A-Z,_]{5})$)";
 	
 }
