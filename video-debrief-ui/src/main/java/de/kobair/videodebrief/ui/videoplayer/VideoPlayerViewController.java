d66f1a930e448546dcdf7f62773f8b6733e6629e
Thorsten Kober <thorsten.kober@softgarden.de>
2019-04-23 15:11:48 +0200

    More prototyping for MediaView

--- video-debrief-ui/src/main/java/de/kobair/videodebrief/ui/videoplayer/VideoPlayerViewController.java
+++ video-debrief-ui/src/main/java/de/kobair/videodebrief/ui/videoplayer/VideoPlayerViewController.java
@@ -15,6 +15,7 @@ import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.effect.DropShadow;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
@@ -34,7 +35,24 @@ public class VideoPlayerViewController implements Initializable {
 	}
 
 	@FXML
-	private AnchorPane anchorPane;
+	private AnchorPane mediaViewAnchorPane;
+	@FXML
+	private Button playPauseButton;
+	@FXML
+	private Button skipButton;
+	@FXML
+	private Button backButton;
+	@FXML
+	private Button nextFrameButton;
+	@FXML
+	private Button previousFrameButton;
+	@FXML
+	private Button fullscreenButton;
+	@FXML
+	private Label timestampLabel;
+	@FXML
+	private Label durationLabel;
+
 
 	private Optional<VideoPlayerDelegate> delegate = Optional.empty();
 	private SelectedMedia selectedMedia;
@@ -43,126 +61,97 @@ public class VideoPlayerViewController implements Initializable {
 		this.delegate = Optional.ofNullable(delegate);
 	}
 
-	public void setSelectedMedia(SelectedMedia selectedMedia) {
-		this.selectedMedia = selectedMedia;
-
+	private Media fxMediaFromSelection(SelectedMedia selectedMedia) {
 		File file = LocalUtils.extendDirectory(selectedMedia.getWorkspace().getWorkspaceDirectory(),
 				selectedMedia.getEvent().getSubPath(), selectedMedia.getPerspective().getFileName());
-
-		// Locate the media content in the CLASSPATH
 		URL mediaUrl;
+
 		try {
 			mediaUrl = file.toURI().toURL();
 		} catch (MalformedURLException e) {
 			e.printStackTrace();
-			return;
+			// TODO: error handling
+			return null;
 		}
-		String mediaStringUrl = mediaUrl.toExternalForm();
 
-		// Create a Media
-		final Media media = new Media(mediaStringUrl);
+		return new Media(mediaUrl.toExternalForm());
+	}
 
-		// Create a Media Player
+	public void setSelectedMedia(SelectedMedia selectedMedia) {
+		this.selectedMedia = selectedMedia;
+		final Media media = fxMediaFromSelection(selectedMedia);
+
 		final MediaPlayer player = new MediaPlayer(media);
-		// Automatically begin the playback
-		player.setAutoPlay(true);
 
-		// Create a 400X300 MediaView
-		final MediaView mediaView = new MediaView(player);
-		mediaView.setFitWidth(400);
-		mediaView.setFitHeight(300);
+		mediaView.setMediaPlayer(player);
 		mediaView.setSmooth(true);
 
-		// Create the DropShadow effect
-		DropShadow dropshadow = new DropShadow();
-		dropshadow.setOffsetY(5.0);
-		dropshadow.setOffsetX(5.0);
-		dropshadow.setColor(Color.WHITE);
-
-		mediaView.setEffect(dropshadow);
-
-		// Create the Buttons
-		Button playButton = new Button("Play");
-		Button stopButton = new Button("Stop");
-
-		// Create the Event Handlers for the Button
-		playButton.setOnAction(new EventHandler<ActionEvent>() {
-			public void handle(ActionEvent event) {
-				if (player.getStatus() == Status.PLAYING) {
-					player.stop();
-					player.play();
-				} else {
-					player.play();
-				}
-			}
-		});
-
-		stopButton.setOnAction(new EventHandler<ActionEvent>() {
-			public void handle(ActionEvent event) {
-				player.stop();
-			}
-		});
-
-		// Create Handlers for handling Errors
-		player.setOnError(new Runnable() {
-			public void run() {
-				// Handle asynchronous error in Player object.
-				printMessage(player.getError());
-			}
-		});
-
-		media.setOnError(new Runnable() {
-			public void run() {
-				// Handle asynchronous error in Media object.
-				printMessage(media.getError());
-			}
-		});
-
-		mediaView.setOnError(new EventHandler<MediaErrorEvent>() {
-			public void handle(MediaErrorEvent event) {
-				// Handle asynchronous error in MediaView.
-				printMessage(event.getMediaError());
-			}
-		});
-
-		// Add a ChangeListener to the player
-		player.statusProperty().addListener(new ChangeListener<MediaPlayer.Status>() {
-			// Log the Message
-			public void changed(ObservableValue<? extends MediaPlayer.Status> ov, final MediaPlayer.Status oldStatus,
-					final MediaPlayer.Status newStatus) {
-				System.out.println("\nStatus changed from " + oldStatus + " to " + newStatus);
-			}
-		});
-
-		// Add a Handler for PLAYING status
-		player.setOnPlaying(new Runnable() {
-			public void run() {
-				System.out.println("\nPlaying now");
-			}
-		});
-
-		// Add a Handler for STOPPED status
-		player.setOnStopped(new Runnable() {
-			public void run() {
-				System.out.println("\nStopped now");
-			}
-		});
-
-		// Create the HBox
-		HBox controlBox = new HBox(5, playButton, stopButton);
-
-		// Create the VBox
-		VBox root = new VBox(5, mediaView, controlBox);
-
-		// Set the Style-properties of the HBox
-		root.setStyle("-fx-padding: 10;" + "-fx-border-style: solid inside;" + "-fx-border-width: 2;"
-				+ "-fx-border-insets: 5;" + "-fx-border-radius: 5;" + "-fx-border-color: blue;");
-
-		anchorPane.getChildren().add(root);
-		AnchorPane.setTopAnchor(root, 0.0);
-		AnchorPane.setLeftAnchor(root, 0.0);
-		AnchorPane.setRightAnchor(root, 0.0);
-		AnchorPane.setBottomAnchor(root, 0.0);
+//		// Create the Buttons
+//		Button playButton = new Button("Play");
+//		Button stopButton = new Button("Stop");
+//
+//		// Create the Event Handlers for the Button
+//		playButton.setOnAction(new EventHandler<ActionEvent>() {
+//			public void handle(ActionEvent event) {
+//				if (player.getStatus() == Status.PLAYING) {
+//					player.stop();
+//					player.play();
+//				} else {
+//					player.play();
+//				}
+//			}
+//		});
+//
+//		stopButton.setOnAction(new EventHandler<ActionEvent>() {
+//			public void handle(ActionEvent event) {
+//				player.stop();
+//			}
+//		});
+//
+//		// Create Handlers for handling Errors
+//		player.setOnError(new Runnable() {
+//			public void run() {
+//				// Handle asynchronous error in Player object.
+//				printMessage(player.getError());
+//			}
+//		});
+//
+//		media.setOnError(new Runnable() {
+//			public void run() {
+//				// Handle asynchronous error in Media object.
+//				printMessage(media.getError());
+//			}
+//		});
+//
+//		mediaView.setOnError(new EventHandler<MediaErrorEvent>() {
+//			public void handle(MediaErrorEvent event) {
+//				// Handle asynchronous error in MediaView.
+//				printMessage(event.getMediaError());
+//			}
+//		});
+//
+//		// Add a ChangeListener to the player
+//		player.statusProperty().addListener(new ChangeListener<MediaPlayer.Status>() {
+//			// Log the Message
+//			public void changed(ObservableValue<? extends MediaPlayer.Status> ov, final MediaPlayer.Status oldStatus,
+//					final MediaPlayer.Status newStatus) {
+//				System.out.println("\nStatus changed from " + oldStatus + " to " + newStatus);
+//			}
+//		});
+//
+//		// Add a Handler for PLAYING status
+//		player.setOnPlaying(new Runnable() {
+//			public void run() {
+//				System.out.println("\nPlaying now");
+//			}
+//		});
+//
+//		// Add a Handler for STOPPED status
+//		player.setOnStopped(new Runnable() {
+//			public void run() {
+//				System.out.println("\nStopped now");
+//			}
+//		});
 	}
 
 	private void printMessage(MediaException error) {
@@ -170,8 +159,10 @@ public class VideoPlayerViewController implements Initializable {
 		System.out.println(errorMessage);
 	}
 
+	@FXML
+	private MediaView mediaView;
+
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-
 	}
 }
