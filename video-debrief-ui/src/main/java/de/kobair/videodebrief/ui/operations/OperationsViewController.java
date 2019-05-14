3e53b95898b45cba9f3ec3508c382cf8f0e0971f
Thorsten Kober <thorsten.kober@softgarden.de>
2019-05-14 15:48:05 +0200

    [UI] first shot for operations list. fixes #6

--- video-debrief-ui/src/main/java/de/kobair/videodebrief/ui/operations/OperationsViewController.java
+++ video-debrief-ui/src/main/java/de/kobair/videodebrief/ui/operations/OperationsViewController.java
@@ -4,13 +4,109 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 import de.kobair.videodebrief.ui.operations.model.LongTermOperation;
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.ReadOnlyDoubleProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.Control;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.VBox;
 
 public class OperationsViewController implements Initializable {
+
+	public class OperationCell extends ListCell<LongTermOperation> {
+
+		private final ReadOnlyDoubleProperty listWidth;
+
+		public OperationCell(ReadOnlyDoubleProperty listWidth) {
+			this.listWidth = listWidth;
+		}
+
+		@Override
+		protected void updateItem(final LongTermOperation operation, final boolean empty) {
+			super.updateItem(operation, empty);
+			if(empty || operation == null) {
+				setText(null);
+				setGraphic(null);
+			} else {
+				/* Header */
+				HBox header = new HBox();
+
+				// Name
+				Label nameLabel = new Label();
+				nameLabel.textProperty().bind(operation.nameProperty);
+				HBox nameContainer = new HBox();
+				nameContainer.setAlignment(Pos.CENTER_LEFT);
+				nameContainer.getChildren().add(nameLabel);
+				header.getChildren().add(nameContainer);
+				HBox.setHgrow(nameContainer, Priority.ALWAYS);
+
+				// Steps
+				if (operation.getTotalSteps() > 1) {
+					Label stepLabel = new Label();
+					String stepsFormat = "%d/" + operation.getTotalSteps();
+					stepLabel.textProperty().bind(operation.stepProperty.asString(stepsFormat));
+					HBox stepsContainer = new HBox();
+					stepsContainer.setAlignment(Pos.CENTER_RIGHT);
+					stepsContainer.getChildren().addAll(stepLabel);
+					header.getChildren().add(stepsContainer);
+				}
+
+				/* Description */
+				HBox description = new HBox();
+				description.setAlignment(Pos.CENTER_LEFT);
+				Label desciptionLabel = new Label();
+				desciptionLabel.textProperty().bind(operation.descriptionProperty);
+				description.getChildren().add(desciptionLabel);
+				HBox.setHgrow(desciptionLabel, Priority.ALWAYS);
+
+				/* Progress */
+				HBox progress = new HBox();
+
+				// Bar
+				ProgressBar progressBar = new ProgressBar();
+				progressBar.progressProperty().bind(operation.progressProperty);
+				HBox progressBarContainer = new HBox();
+				progressBarContainer.setAlignment(Pos.CENTER_LEFT);
+				progressBarContainer.getChildren().add(progressBar);
+				progress.getChildren().add(progressBarContainer);
+				HBox.setHgrow(progressBarContainer, Priority.ALWAYS);
+
+				// Label
+				Label percentLabel = new Label();
+				percentLabel.textProperty();
+				operation.progressProperty.addListener((observable, oldValue, newValue) -> {
+					int percent = (int) (newValue.doubleValue() * 100);
+					if (percent >= 0) {
+						percentLabel.setText(String.format("%d%%", percent));
+					} else {
+						percentLabel.setText(null);
+					}
+				});
+				HBox percentContainer = new HBox();
+				percentContainer.setAlignment(Pos.CENTER_RIGHT);
+				percentContainer.getChildren().add(percentLabel);
+				progress.getChildren().add(percentContainer);
+
+				VBox content = new VBox();
+				content.getChildren().addAll(header, description, progress);
+				this.setGraphic(content);
+				this.setText(null);
+
+				this.prefWidthProperty().bind(this.listWidth.subtract(2));
+				this.setMaxWidth(Control.USE_PREF_SIZE);
+			}
+		}
+	}
 
 	private ObservableList<LongTermOperation> operations = FXCollections.observableArrayList();
 
@@ -24,6 +120,7 @@ public class OperationsViewController implements Initializable {
 	@Override
 	public void initialize(final URL location, final ResourceBundle resources) {
 		this.operationsListView.setItems(operations);
+		this.operationsListView.setCellFactory(listView -> new OperationCell(listView.widthProperty()));
 	}
 
 }
--- video-debrief-ui/src/main/java/de/kobair/videodebrief/ui/operations/model/LongTermOperation.java
+++ video-debrief-ui/src/main/java/de/kobair/videodebrief/ui/operations/model/LongTermOperation.java
@@ -1,7 +1,10 @@
 package de.kobair.videodebrief.ui.operations.model;
 
 import de.kobair.videodebrief.core.operations.Operation;
+import javafx.application.Platform;
+import javafx.beans.property.DoubleProperty;
 import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleDoubleProperty;
 import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
@@ -9,26 +12,39 @@ import javafx.beans.property.StringProperty;
 public class LongTermOperation implements Operation {
 
 	public StringProperty nameProperty;
-	public StringProperty currentStepProperty;
-	public IntegerProperty progressProperty;
+	public StringProperty descriptionProperty;
+	public DoubleProperty progressProperty;
+	public IntegerProperty stepProperty;
+
+	private final int totalSteps;
 
 	public LongTermOperation(String name, String currentStep) {
-		this(name, currentStep, -1);
+		this(name, currentStep, -1.0, 1, 1);
 	}
 
-	public LongTermOperation(String name, String currentStep, int progress) {
+	public LongTermOperation(String name, String currentStep, double progress, int step, int totalSteps) {
 		this.nameProperty = new SimpleStringProperty(name);
-		this.currentStepProperty = new SimpleStringProperty(currentStep);
-		this.progressProperty = new SimpleIntegerProperty(progress);
+		this.descriptionProperty = new SimpleStringProperty(currentStep);
+		this.progressProperty = new SimpleDoubleProperty(progress);
+		this.stepProperty = new SimpleIntegerProperty(step);
+		this.totalSteps = totalSteps;
+	}
+
+	public int getTotalSteps() {
+		return totalSteps;
 	}
 
 	@Override
-	public void updateCurrentStep(final String currentStep) {
-		this.currentStepProperty.set(currentStep);
+	public void updateDescription(final String desciption) {
+		Platform.runLater(() -> {
+			this.descriptionProperty.set(desciption);
+		});
 	}
 
 	@Override
-	public void updateProgress(final int progress) {
-		this.progressProperty.set(progress);
+	public void updateProgress(final double progress) {
+		Platform.runLater(() -> {
+			this.progressProperty.set(progress);
+		});
 	}
 }
