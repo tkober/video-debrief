dc1f27e4a29d0f3b87a35a8e1f854cf4aa145b80
Thorsten Kober <thorsten.kober@softgarden.de>
2019-05-24 08:54:13 +0200

    [CORE] Added smart file size to LocalUtils.

--- video-debrief-core/src/main/java/de/kobair/videodebrief/core/utils/LocalUtils.java
+++ video-debrief-core/src/main/java/de/kobair/videodebrief/core/utils/LocalUtils.java
@@ -80,4 +80,80 @@ public class LocalUtils {
 	public static void copyFile(File source, File destination, ProgressCallback progressCallback) throws IOException {
 		startCopyJob(source, destination, Optional.ofNullable(progressCallback));
 	}
+
+	public enum FileSizeAccuracy {
+		TWO_DECIMALS(2),
+		NO_DECIMALS(0);
+
+		private final int decimals;
+
+		FileSizeAccuracy(final int decimals) {
+			this.decimals = decimals;
+		}
+
+		public int getDecimals() {
+			return decimals;
+		}
+	}
+
+	@FunctionalInterface
+	private interface FileSizeFormatter {
+		String format(String value);
+	}
+
+	public enum FileSizeUnitFormat {
+		DEFAULT(value -> value),
+		UPPER_CASE(value -> value.toUpperCase()),
+		LOWER_CASE(value -> value.toLowerCase());
+
+		private final FileSizeFormatter formatter;
+
+		FileSizeUnitFormat(final FileSizeFormatter formatter) {
+			this.formatter = formatter;
+		}
+
+		protected FileSizeFormatter getFormatter() {
+			return formatter;
+		}
+	}
+
+	public static String smartFileSizeString(long sizeBytes) {
+		return smartFileSizeString(sizeBytes, FileSizeAccuracy.TWO_DECIMALS, FileSizeUnitFormat.DEFAULT);
+	}
+
+	public static String smartFileSizeString(long sizeBytes, FileSizeAccuracy accuracy, FileSizeUnitFormat format) {
+		final long KILOBYTE = 1<<10;
+		final long MEGABYTE = KILOBYTE<<10;
+		final long GIGABYTE = MEGABYTE<<10;
+
+		if (sizeBytes > GIGABYTE) {
+			double sizeGigabyte = sizeBytes / (double) GIGABYTE;
+			return formatSize(sizeGigabyte, "GB", accuracy, format);
+		}
+
+		if (sizeBytes > MEGABYTE) {
+			double sizeMegabyte = sizeBytes / (double) MEGABYTE;
+			return formatSize(sizeMegabyte, "MB", accuracy, format);
+		}
+
+		if (sizeBytes > KILOBYTE) {
+			double sizeKilobyte = sizeBytes / (double) KILOBYTE;
+			return formatSize(sizeKilobyte, "KB", accuracy, format);
+		}
+
+		return formatSize(sizeBytes, "bytes", accuracy, format);
+	}
+
+	private static String formatSize(double size, String unit, FileSizeAccuracy accuracy, FileSizeUnitFormat format) {
+		String sizeString = stringWithDecimals(size, accuracy.getDecimals());
+		String result = String.format("%s %s", sizeString, unit);
+		return format.getFormatter().format(result);
+	}
+
+	private static String stringWithDecimals(double value, int decimals) {
+		String format = "%." + decimals + "f";
+		return String.format(format, value);
+	}
+
+
 }
